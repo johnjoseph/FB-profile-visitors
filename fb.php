3332bbc2fcda118ede0c2f20c38cbb98d5b7d37c
johnjoseph <johnpjoseph91@gmail.com>
2014-02-21 13:29:36 +0530

    adding files

--- fb.php
+++ fb.php
@@ -1,9 +1,14 @@
 <?php
+/* from the source code of your facebook profile, search for 'FriendsList', and you can see a csv format of fb profile ids of the form
+   "xxxxxxxxxx-y","xxxxxxxxxx-y" copy it onto a file and name it fb.csv.
+   specify the lower and upper limits (preferably in diff of 50) to avoid your browser form hanging from processing too many queries */
+$lower=0;
+$upper=50;
 $file=fopen('fb.csv','r');
 $fgetcsv=fgetcsv($file);
 foreach ($fgetcsv as $key => $value) 
 {
-	if($key>600&&$key<650)
+	if($key>$lower&&$key<$upper)
 	{
 	$val=explode('-',$value);
 	$fget=json_decode(file_get_contents("https://graph.facebook.com/".$val[0]."/"));
